da92612b96055bb985f345a95cf0064fff5eb371
Lionel TAILHARDAT <lionel.tailhardat@orange.com>
2024-02-21 07:27:13 +0100

    Added logging and code documentation.

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ import time
 from argparse import ArgumentParser
 from os import path
 from pathlib import Path
+import logging
 
 import yaml
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
@@ -48,10 +49,24 @@ available_llms = {
 
 # device = "cuda"
 
+# ----------------------------------------------------------------------------
+
+logBaseName = "eurecom.llm4ke.prompt"
+logFileName = logBaseName + ".log"
+
+loggingFormatString = (
+    "%(asctime)s:%(levelname)s:%(threadName)s:%(funcName)s:%(message)s"
+)
+
 
 # === Functions ===============================================================
 
 def simplify(uri):
+    """
+    Remove useless parts of the URI, such as the namespace, for easier processing.
+    :param uri: an URI as a string
+    :return: a simpler URI as a string
+    """
     return re.split(r'[#/]', uri)[-1].replace('_', ' ')
 
 
@@ -100,19 +115,26 @@ def run(task, input_path, llm_model, ont_name,
     :param n_examples:
     :return:
     """
+
+    # Loading the ontology
     g = Graph()
     for g_path in os.listdir(path.join(input_path, 'dm')):
         if not g_path.split('.')[-1] in ['rdf', 'ttl', 'owl']:
             continue
-        g.parse(path.join(input_path, 'dm', g_path))
+        dm_path = path.join(input_path, 'dm', g_path)
+        logging.debug("LOAD:ONTOLOGY:PARSING:dm_path=%s", dm_path)
+        g.parse(dm_path)
 
+    # Retrieving classes and properties as lists
     cls = [s for s, p, o in g.triples((None, RDF.type, OWL.Class))]
     props = ([s for s, p, o in g.triples((None, RDF.type, OWL.ObjectProperty))] +
              [s for s, p, o in g.triples((None, RDF.type, OWL.DatatypeProperty))])
 
     description = Path(path.join(input_path, 'description.txt')).read_text() if include_description else ''
 
-    print(f'The {ont_name} ontology has {len(cls)} classes and {len(props)} properties')
+    logging.info("LOAD:ONTOLOGY:STATS:The %s ontology has %s classes and %s properties.", ont_name, len(cls), len(props))
+    # print(f'The {ont_name} ontology has {len(cls)} classes and {len(props)} properties')
+
     schema = []
     for p in props:
         domain = g.value(p, RDFS.domain, None)
@@ -124,12 +146,16 @@ def run(task, input_path, llm_model, ont_name,
             else:
                 schema.append((simplify(domain), simplify(p), 'literal'))
 
-    PROMPT_TEMPLATE = LocalTemplate.load(f'./src/prompt_templates/{task}.yml')
+    template_path = f'./src/prompt_templates/{task}.yml'
+    PROMPT_TEMPLATE = LocalTemplate.load(template_path)
+    logging.debug("LOAD:PROMPT_TEMPLATE:template_path=%s:template=%s", template_path, PROMPT_TEMPLATE)
 
+    # Retrieve the essential information from the schema
     classes_batches = [[simplify(elm) for elm in class_set] for class_set in select_in_batches(cls)]
     property_batches = [flatten([select_props_by_class(schema, cl) for cl in batch]) for batch in classes_batches]
     schema_batches = [flatten([select_schema_by_class(schema, cl) for cl in batch]) for batch in classes_batches]
 
+    # Get Competency Question examples from the dataset
     examples = ''
     if n_examples > 0:
         with open(path.join(input_path, 'cqs', 'cqs.yml')) as f:
@@ -137,7 +163,9 @@ def run(task, input_path, llm_model, ont_name,
         cqs = [c['question'] for c in ground_truth['ontology']['cqs']]
 
         examples = 'For example:' + '\n -'.join(cqs[:n_examples])
+    logging.debug("LOAD:CQS:n_examples=%s:examples=%s", n_examples, examples)
 
+    # Generate prompts
     input_batches = []
     for c_batch, p_batch, s_batch in zip(classes_batches, property_batches, schema_batches):
         ont_input = {
@@ -153,7 +181,9 @@ def run(task, input_path, llm_model, ont_name,
         for x in PROMPT_TEMPLATE.input:
             input_dict[x] = ont_input[x]
         input_batches.append(input_dict)
+    logging.debug("PROMPT:PREPARE:input_batches=%s", input_batches)
 
+    # Instanciate LLM
     if local_llm or available_llms[llm_model] == 'local':
         llm = Ollama(model=llm_model)
     else:
@@ -181,10 +211,13 @@ def run(task, input_path, llm_model, ont_name,
     )
     chain = prompt | llm | output_parser
 
+    # Call LLM
+    logging.info("PROMPT:CALL_LLM:%s", 'START')
     config = {"callbacks": [CustomHandler()]} if verbose else {}
     res = chain.batch(input_batches, config=config)
+    logging.info("PROMPT:CALL_LLM:%s:res=%s", 'DONE', res)
 
-    # parse output
+    # Parse output to extract Competency Questions
     patterns = [r'(\d+)\. (.+)[?.]', r'\s*-\s*(.*?)[?.]']
     cqs = []
 
@@ -197,12 +230,22 @@ def run(task, input_path, llm_model, ont_name,
                         cqs.append(match[1])
                     else:
                         cqs.append(match)
+    logging.debug("PROMPT:PROCESS_LLM_RESPONSE:len(cqs)=%s", len(cqs))
 
-    os.makedirs(path.join(output_path, ont_name), exist_ok=True)
+    # Prepare the file system for saving the results
+    output_folder = path.join(output_path, ont_name)
+    os.makedirs(output_folder, exist_ok=True)
+    logging.info("SAVE:RESULTS:output_folder=%s", output_folder)
+
+    # Saving the results to a file
     id = id if id is not None else time.time()
-    with open(path.join(output_path, ont_name, f'{ont_name}_{id}.txt'), 'w') as f:
+    output_file = path.join(output_path, ont_name, f'{ont_name}_{id}.txt')
+    with open(output_file, 'w') as f:
         f.write('\n'.join(cqs))
-    print(res)
+    logging.info("SAVE:RESULTS:output_file=%s", output_file)
+
+    # End of run
+    # print(res)
     return True, res
 
 
@@ -215,22 +258,88 @@ if __name__ == '__main__':
     # Define argument parser
     parser = ArgumentParser(
         prog='LLM4ke',
-        description='Experiment about LLMs on Knowledge Graphs')
-    parser.add_argument('task', choices=available_tasks)
-    parser.add_argument('-i', '--input', help='Input folder', default='./data/Odeuropa/')
-    parser.add_argument('-o', '--output', help='Output folder', default='./out/')
-    parser.add_argument('--local_llm', action='store_true', help='If True, use Ollama instead of Hugging Face')
-    parser.add_argument('--llm', help='LLM to use', default='llama2', choices=available_llms)
-    parser.add_argument('--name', help='Name of the ontology', required=True)
-    parser.add_argument('-n', '--n_cqs', help='Number of competency questions to get in output',
-                        type=int, default=10)
-    parser.add_argument('-x', '--n_examples', help='Number of example competency questions to provide in input',
-                        type=int, default=0)
-    parser.add_argument('--include_description', help='Include the content of description.txt in the prompt',
-                        default=False, action='store_true')
-    parser.add_argument('--verbose', help='Print the full prompt',
-                        default=False, action='store_true')
-    parser.add_argument('--id', help='Id for naming the output file. If absent, it will be a timestamp')
+        description='Experiment about LLMs on Knowledge Graphs'
+    )
+    parser.add_argument(
+        'task',
+        choices=available_tasks
+    )
+
+    parser.add_argument(
+        '-i',
+        '--input',
+        help='Input folder',
+        default='./data/Odeuropa/'
+    )
+
+    parser.add_argument(
+        '-o',
+        '--output',
+        help='Output folder',
+        default='./out/'
+    )
+    parser.add_argument(
+        '--local_llm',
+        action='store_true',
+        help='If True, use Ollama instead of Hugging Face'
+    )
+
+    parser.add_argument(
+        '--llm',
+        help='LLM to use',
+        default='llama2',
+        choices=available_llms
+    )
+
+    parser.add_argument(
+        '--name',
+        help='Name of the ontology',
+        required=True
+    )
+
+    parser.add_argument(
+        '-n',
+        '--n_cqs',
+        help='Number of competency questions to get in output',
+        type=int,
+        default=10
+    )
+
+    parser.add_argument(
+        '-x',
+        '--n_examples',
+        help='Number of example competency questions to provide in input',
+        type=int,
+        default=0
+    )
+
+    parser.add_argument(
+        '--include_description',
+        help='Include the content of description.txt in the prompt',
+        default=False,
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '--verbose',
+        help='Print the full prompt',
+        default=False,
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '--id',
+        help='Id for naming the output file. If absent, it will be a timestamp'
+    )
+
+    parser.add_argument(
+        "--log",
+        type=int,
+        choices=[10, 20, 30, 40, 50],
+        action="store",
+        default=20,
+        help="Verbosity (default: INFO) : DEBUG = 10, INFO = 20, WARNING = 30, ERROR = 40, CRITICAL = 50",
+    )
 
     # Instanciate argument parser
     args = parser.parse_args()
@@ -240,6 +349,23 @@ if __name__ == '__main__':
         if selected != "local":
             _llm = selected
 
+    # Configure logger
+    logging.basicConfig(format=loggingFormatString, level=args.log)
+
     # Call the processing function
-    run(args.task, args.input, _llm, args.name, args.n_cqs, args.include_description, args.verbose, args.output,
-        args.id, args.local_llm, args.n_examples)
+    logging.info("INIT")
+    run_response = run(
+        args.task,
+        args.input,
+        _llm,
+        args.name,
+        args.n_cqs,
+        args.include_description,
+        args.verbose,
+        args.output,
+        args.id,
+        args.local_llm,
+        args.n_examples
+    )
+
+    logging.info("DONE")
