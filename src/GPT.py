511679c75442a394645290a5ae5de0a6f98bc618
Youssra REBBOUD <100802809+RYoussra@users.noreply.github.com>
2024-02-22 09:23:10 +0100

    parse update

--- src/GPT.py
+++ src/GPT.py
@@ -1,31 +1,17 @@
 import os
-import wandb
-from openai import OpenAI
-client = OpenAI()
-gpt_assistant_prompt = "You are a " + input ("Who should I be, as I answer your prompt?")
-gpt_user_prompt = input ("What prompt do you want me to do?")
-gpt_prompt = gpt_assistant_prompt, gpt_user_prompt
-print(gpt_prompt)
-message=[{"role": "assistant", "content": gpt_assistant_prompt}, {"role": "user", "content": gpt_user_prompt}]
-temperature=0.2
-max_tokens=256
-frequency_penalty=0.0
+# get a token: https://platform.openai.com/account/api-keys
 
+from getpass import getpass
 
-response = client.chat.completions.create(
-    model="gpt-4",
-    messages = message,
-    temperature=temperature,
-    max_tokens=max_tokens,
-    frequency_penalty=frequency_penalty
-)
-print(response.choices[0].message)
-wandb.init()
-response_text = response.choices[0].message.content
-tokens_used = response.usage.total_tokens
+OPENAI_API_KEY = os.getenv("key")
 
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_openai import OpenAI
+template = """Question: {question}
 
-prediction_table = wandb.Table(columns=["Prompt", "Response", "Tokens", "Max Tokens", "Frequency Penalty", "Temperature"])
-prediction_table.add_data(gpt_prompt, response_text, tokens_used, max_tokens, frequency_penalty, temperature)
-wandb.log({'predictions': prediction_table})
-wandb.finish()
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate.from_template(template)
+llm = OpenAI()
+llm_chain = LLMChain(prompt=prompt, llm=llm)
